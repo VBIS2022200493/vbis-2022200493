7ee0eb924a398820b77b5a557cfd3923ebbdb164
MarkovicMihajlo <mihajlomarkovic003@gmail.com>
2024-11-13 21:55:03 +0100

    Promena u UserController

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,11 +2,15 @@
     namespace app\controllers;
     class UserController{
         public function userCreate(){
+
             return "User Created";
+
         }
 
         public function readUser(){
+
             return "User";
+
         }
 
     }
