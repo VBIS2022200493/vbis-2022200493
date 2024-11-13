b614d76cf59bd9318d4363baa96d1d3b62e245a3
MarkovicMihajlo <mihajlomarkovic003@gmail.com>
2024-11-13 11:06:52 +0100

    Postavljam promenu

---
+++ controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+    namespace app\controllers;
+    class UserController{
+        public function userCreate(){
+            return "User Created";
+        }
+
+        public function readUser(){
+            return "User";
+        }
+
+    }
